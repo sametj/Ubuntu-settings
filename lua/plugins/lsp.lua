47025605fcac9866f67668a062b627b1ea6ace12
sametj <mosesjames2000@gmail.com>
2025-05-29 14:03:18 -0400

    updates lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -81,61 +81,6 @@ return {
       ts_ls = function()
         return true -- disables ts_ls setup
       end,
-      vtsls = function(_, opts)
-        -- Attach your custom commands and extend settings here if needed
-        LazyVim.lsp.on_attach(function(client, buffer)
-          client.commands['_typescript.moveToFileRefactoring'] = function(command, ctx)
-            local action, uri, range = unpack(command.arguments)
-            local function move(newf)
-              client.request('workspace/executeCommand', {
-                command = command.command,
-                arguments = { action, uri, range, newf },
-              })
-            end
-
-            local fname = vim.uri_to_fname(uri)
-            client.request('workspace/executeCommand', {
-              command = 'typescript.tsserverRequest',
-              arguments = {
-                'getMoveToRefactoringFileSuggestions',
-                {
-                  file = fname,
-                  startLine = range.start.line + 1,
-                  startOffset = range.start.character + 1,
-                  endLine = range['end'].line + 1,
-                  endOffset = range['end'].character + 1,
-                },
-              },
-            }, function(_, result)
-              local files = result.body.files
-              table.insert(files, 1, 'Enter new path...')
-              vim.ui.select(files, {
-                prompt = 'Select move destination:',
-                format_item = function(f)
-                  return vim.fn.fnamemodify(f, ':~:.')
-                end,
-              }, function(f)
-                if f and f:find '^Enter new path' then
-                  vim.ui.input({
-                    prompt = 'Enter move destination:',
-                    default = vim.fn.fnamemodify(fname, ':h') .. '/',
-                    completion = 'file',
-                  }, function(newf)
-                    if newf then
-                      move(newf)
-                    end
-                  end)
-                elseif f then
-                  move(f)
-                end
-              end)
-            end)
-          end
-        end, 'vtsls')
-
-        -- Merge typescript settings to javascript
-        opts.settings.javascript = vim.tbl_deep_extend('force', {}, opts.settings.typescript, opts.settings.javascript or {})
-      end,
     },
   },
 }
