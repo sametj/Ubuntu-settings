599fc5ccdd37636322740bfc98e792fc54992f0a
sametj <mosesjames2000@gmail.com>
2025-05-30 16:06:00 -0400

    updated obsidian plugin

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -29,13 +29,6 @@ return {
       },
     },
 
-    completion = {
-      -- Set to false to disable completion.
-      nvim_cmp = true,
-      -- Trigger completion at 2 chars.
-      min_chars = 2,
-    },
-
     mappings = {
       -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
       ['gf'] = {
